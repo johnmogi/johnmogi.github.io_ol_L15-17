a01f4700359d423bab0f13ee6aa802a3ff35c046
johnmogi <anguru@gmail.com>
2019-09-14 18:28:39 +0300

    finishe_manual Up

---
+++ L16/Pr1/HW/e2/e2/libs/elements.js
@@ -0,0 +1,9 @@
+const stage = document.getElementById("stage")
+const loginmein = document.getElementById("loginmein")
+const input1 = document.getElementById("input1")
+const input2 = document.getElementById("input2")
+const rem = document.getElementById("rem")
+const h5 = document.getElementsByTagName("h5")
+
+const sub = document.getElementById("sub")
+const inputs = document.getElementsByTagName("input")
