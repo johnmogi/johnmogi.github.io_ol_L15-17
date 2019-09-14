e342a09f961229e972a465784f4b66208028c2a0
johnmogi <anguru@gmail.com>
2019-09-14 19:03:35 +0300

    MU e3

---
+++ L16/Pr1/HW/e3/e2.js
@@ -0,0 +1,54 @@
+
+
+// clean the slate
+function init(){
+    for (let i = 0; i < inputs.length; i++) {
+        inputs[i].value = ""
+    }
+}
+// start buttons
+window.addEventListener("load", init)
+sub.addEventListener("click", draw);
+
+function memory(){
+    if (autoSizingCheck2.checked == true){
+        h5[0].innerText = ("I got you in my memory- Have a nice day")
+
+        if (input1.value == "" || input2.value == "") {
+       return 
+               
+    } 
+
+    localStorage.setItem( 'inputs', JSON.stringify( arr ) );
+    // localStorage.setItem('userName', inputs[0].value, );
+    // localStorage.setItem('PassWord', inputs[1].value, );
+
+        
+    }
+}
+//generate
+function draw(){
+
+    function error(){
+        stage.style.color = ("#f00");
+        stage.innerText = ("One of the fields is empty, try again")
+        return
+    }
+function success(){
+    if (input1.value == "good" && input2.value == "day") {
+        stage.style.color = ("#fff");
+        stage.innerText = ("Good Day!") 
+        window.location.assign("success.html")
+           
+    } 
+    else {
+        stage.innerText = ("you can't go in....")
+    }
+}
+    if (input1.value == "" || input2.value == "") {
+        error();   
+           
+} 
+memory();
+success();
+}
