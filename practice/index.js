208aaaab207b965eb99a5ee0905e39c85fe98afc
johnmogi <anguru@gmail.com>
2019-09-18 08:41:24 +0300

    trying to upload the new answer

---
+++ practice/index.js
@@ -0,0 +1,30 @@
+// input1.style.border=("#ff0")
+
+// calculate
+// *rect 
+//  <rect width="400" height="100" style="fill:rgb(0,0,255);stroke-width:10;stroke:rgb(0,0,0)" />
+
+
+// draw => svg
+
+const rectRekt = document.getElementById("rectRekt")
+const stage = document.getElementById("stage")
+
+const sub = document.getElementById("sub")
+const svg = document.getElementById("svg")
+const input1 = document.getElementById("input1")
+
+const rect= document.createElement("rect") // create div
+svg.append(rect) // stage
+// const newDiv= 
+// const valueIn = input1.value
+
+sub.addEventListener("click", calCulate)
+
+function calCulate(){
+//    const rect = svg.createElement("rect")
+
+}
+
+
+
